10f3e4ec315e6fe352962416a671820246511a78
jrw <joshr.warwick@gmail.com>
2025-08-08 22:32:40 +0100

    Add populate and shuffle methods alongside additional print methods used for testing

--- src/main/java/cards/StandardDeck.java
+++ src/main/java/cards/StandardDeck.java
@@ -1,6 +1,7 @@
 package cards;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StandardDeck {
@@ -8,15 +9,45 @@ public class StandardDeck {
 
     public StandardDeck(){
         cards = new ArrayList<>();
-    }
-
-    //need to implement a loop to generate a standard 52 card deck
-    private void populateDeck(){
-        var card = new Card(Card.Suit.DIAMONDS, 5);
-        cards.add(card);
+        populateDeck();
     }
 
     public List<Card> getCards() {
         return cards;
+    }
+
+    //Populates a standard 52-card deck
+    private void populateDeck(){
+        for (Card.Suit suit: Card.Suit.values()){
+            for(int i = 1; i <=13; i++){
+                cards.add(new Card(suit, i));
+            }
+        }
+    }
+
+    public void printShortFormDeck(){
+        for (int i = 0; i < cards.size(); i++){
+            System.out.print(cards.get(i).shortForm() + " ");
+            if((i+1) % 13 == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    //Prints the deck separated into suits
+    //Only really used to test that all cards are populated correctly
+    public void printBySuit(){
+        Card.Suit currentSuit = cards.getFirst().getSuit();
+        for (Card card: cards){
+            if (!(card.getSuit().equals(currentSuit))){
+                System.out.println();
+                currentSuit = card.getSuit();
+            }
+            System.out.println(card);
+        }
+    }
+
+    public void shuffleDeck(){
+        Collections.shuffle(cards);
     }
 }
