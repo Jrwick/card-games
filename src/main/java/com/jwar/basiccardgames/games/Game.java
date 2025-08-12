f541ae240bff09514fa3929e668a416df02b5194
jrw <joshr.warwick@gmail.com>
2025-08-12 13:48:32 +0100

    Create Game superclass and Rummy subclass

---
+++ src/main/java/com/jwar/basiccardgames/games/Game.java
@@ -0,0 +1,61 @@
+package com.jwar.basiccardgames.games;
+
+import com.jwar.basiccardgames.decks.EmptyDeck;
+import com.jwar.basiccardgames.decks.StandardDeck;
+import com.jwar.basiccardgames.players.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public abstract class Game {
+
+    protected final List<Player> players;
+    protected final StandardDeck deck;
+    protected final EmptyDeck discardPile;
+    protected Player currentPlayer;
+
+    public Game(StandardDeck deck){
+        this.deck = deck;
+        this.players = new ArrayList<>();
+        this.discardPile = new EmptyDeck();
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public StandardDeck getDeck() {
+        return deck;
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
+    }
+
+    public void deal(Player player, int numOfCards){
+        for (int i = 0; i < numOfCards; i++){
+            player.addCardToHand(this.deck);
+        }
+    }
+
+    public Player getFirstPlayer(){
+        Random ran = new Random();
+        if (players.isEmpty()){
+            return null;
+        } else {
+            return players.get(ran.nextInt(players.size()));
+        }
+    }
+
+    public abstract void startGame();
+
+    @Override
+    public String toString() {
+        ArrayList<String> names = new ArrayList<>();
+        for (Player player: players){
+            names.add(player.getName());
+        }
+        return players.size() + " players: " + String.join(", ", names);
+    }
+}
---
+++ src/main/java/com/jwar/basiccardgames/games/Rummy.java
@@ -0,0 +1,34 @@
+package com.jwar.basiccardgames.games;
+
+import com.jwar.basiccardgames.decks.StandardDeck;
+import com.jwar.basiccardgames.players.Player;
+
+public class Rummy extends Game {
+
+    private final static int HAND_LIMIT = 7;
+
+    public Rummy(StandardDeck deck) {
+        super(deck);
+    }
+
+    @Override
+    public void startGame() {
+        this.getDeck().shuffleDeck();
+        initialDeal();
+        currentPlayer = getFirstPlayer();
+        currentPlayer.addCardToHand(deck);
+    }
+
+    private void initialDeal(){
+        for (Player player: players){
+            deal(player, HAND_LIMIT);
+        }
+    }
+
+
+
+    @Override
+    public String toString() {
+        return super.toString() + " playing Rummy";
+    }
+}
