f68e13502cab63d573d1f1b6915b388396dfc85d
jrw <joshr.warwick@gmail.com>
2025-08-12 13:49:03 +0100

    Create Player class

---
+++ src/main/java/com/jwar/basiccardgames/players/Player.java
@@ -0,0 +1,35 @@
+package com.jwar.basiccardgames.players;
+
+import com.jwar.basiccardgames.cards.Card;
+import com.jwar.basiccardgames.decks.StandardDeck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private final List<Card> hand;
+    private final String name;
+
+    public Player (String name){
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addCardToHand(StandardDeck deck){
+        hand.add(deck.dealTopCard());
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + " holding " + this.hand.size() + " cards";
+    }
+}
